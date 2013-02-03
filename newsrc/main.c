0aff32c017e86fa769c95533de49a9a87c2da49c
Jon Aylard <jonathan.aylard@hexus.net>
2013-02-03 22:43:01 +0100

    interface is now awesome

--- newsrc/main.c
+++ newsrc/main.c
@@ -1,3 +1,5 @@
+#include "interface.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -6,43 +8,6 @@
 #include <time.h>
 #include <inttypes.h>
 
-WINDOW *iface, *tt;
-
-void *terminal() {
-	//essential ncurses setup
-        initscr();
-        noecho();
-	cbreak();
-
-	// newwin(int height, int width, int starty, int startx)
-	tt = newwin(0, 0, 0, 0); //create full screen window for teletype
-	iface = newwin(LINES - 1, COLS - 1, 1, 1); //create full screen window for interface
-
-        scrollok(iface, 1); //allow window to scroll
-        scrollok(tt, 1);
-
-	box(iface, 0, 0);
-
-	wmove(iface, 1, 1);
-	waddstr(iface, "iface window\n");
-	refresh();
-        touchwin(iface);
-        wrefresh(iface);
-
-        char ch;
-        while(ch != 27) {
-                ch = getch();
-		if(ch == 'n') {
-			waddstr(iface, "\n n \n");
-			wrefresh(iface);
-		}
-		else {
-			waddstr(iface, "\n key pressed \n");
-			wrefresh(iface);
-		}
-	}
-	pthread_exit(NULL);
-}
 
 int INSTRUCTIONS_PER_SEC = 300000;
 int NSLEEP_EVERY = 3000;
@@ -61,6 +26,7 @@ int main(int argc, const char* argv[]) {
 	long ns_per_every = 0;
 	clock_gettime(CLOCK_MONOTONIC, &start_time);
 	while(1) {
+		{ //run at a certain number of instructions per seconds
 		instruction_count++;
 		if(instruction_count % INSTRUCTIONS_PER_SEC == 0 && ns_per_every == 0) {
 			clock_gettime(CLOCK_MONOTONIC, &end_time);
@@ -80,6 +46,8 @@ int main(int argc, const char* argv[]) {
 		if(instruction_count % NSLEEP_EVERY == 0 && ns_per_every > 0) {
 			nanosleep(&req, &rem);
 		}
+		}
+		
 	}
 
 	return (EXIT_SUCCESS);
